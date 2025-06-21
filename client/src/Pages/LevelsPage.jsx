3b0ddfc4153e4862bb83b3120832328e8b6681aa
amohan179 <157762058+amohan179@users.noreply.github.com>
2025-06-21 12:56:02 -0400

    Set the levels page
    Officially set the levels page with backgrounds

--- client/src/Pages/LevelsPage.jsx
+++ client/src/Pages/LevelsPage.jsx
@@ -1,3 +1,4 @@
+import bgImage from "../assets/blue background.jpeg";
 import React from "react";
 
 import { styled } from '@mui/material/styles';
@@ -6,6 +7,14 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { Typography } from "@mui/material";
+import level1 from"../assets/Level1.avif"
+import level2 from "../assets/Level2.jpg"
+import level3 from "../assets/Level3.jpg"
+import level4 from "../assets/Level4.webp"
+import level5 from "../assets/Level5.png"
+import level6 from "../assets/Level6.gif"
+import level7 from "../assets/Level7.jpg"
+import level8 from "../assets/Level8.jpg"
 
 
 
@@ -46,8 +55,8 @@ export default function LevelsPage({ columns = 4 }) {
   };
 
   const boxBase = {
-    height: "275px",
-    width: "200px",
+    height: "400px",
+    width: "450px",
     borderRadius: "24px",
     display: "flex",
     alignItems: "center",
@@ -59,20 +68,33 @@ export default function LevelsPage({ columns = 4 }) {
 
   // Color definitions for each level
   const levels = [
-    { label: "Level 1", bg: "#FCA5A5", txt: "#58151C" },
-    { label: "Level 2", bg: "#FCD29A", txt: "#603C1D" },
-    { label: "Level 3", bg: "#FDE68A", txt: "#4A351B" },
-    { label: "Level 4", bg: "#BBF7D0", txt: "#14532D" },
-    { label: "Level 5", bg: "#FCA5A5", txt: "#58151C" },
-    { label: "Level 6", bg: "#FCD29A", txt: "#603C1D" },
-    { label: "Level 7", bg: "#FDE68A", txt: "#4A351B" },
-    { label: "Level 8", bg: "#BBF7D0", txt: "#14532D" },
+    { label: "Level 1", bg: "#FCA5A5", bgImage:level1 },
+    { label: "Level 2", bg: "#FCD29A", bgImage:level2},
+    { label: "Level 3", bg: "#FDE68A", bgImage:level3},
+    { label: "Level 4", bg: "#BBF7D0", bgImage:level4 },
+    { label: "Level 5", bg: "#FCA5A5", bgImage:level5},
+    { label: "Level 6", bg: "#FCD29A", bgImage:level6},
+    { label: "Level 7", bg: "#FDE68A", bgImage:level7},
+    { label: "Level 8", bg: "#BBF7D0", bgImage:level8},
   ];
 
   return (
-    <Container maxWidth="lg"  align="center">
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{
+        backgroundColor: "#1a1a1a",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        width: "100%",
+        m: 0,
+        p: 0
+      }}
+    >
       {/* Header Bar */}
-      <Typography variant="h1" gutterBottom align={"center"}>
+      <Typography variant="h1" gutterBottom align={"center"} color="white">
         Levels
       </Typography>
 
@@ -82,14 +104,14 @@ export default function LevelsPage({ columns = 4 }) {
         sx={{ padding: '60px', marginTop: '20px' }}
       >
         {levels.map((level) => (
-          <Grid item size={3} xs={4} key={level.label} display={"flex"} flexDirection={"column"}>
+          <Grid item size={3} xs={4} key={level.label} display={"flex"} flexDirection={"column"} paddingBottom={2} paddingTop={2}>
             
-            <Typography>{level.label} </Typography>
+            <Typography color="whitesmoke" textAlign={"center"} fontSize={36}>{level.label} </Typography>
             <Box
               sx={{
+                backgroundImage: `url(${level.bgImage})`,
                 ...boxBase,
                 backgroundColor: level.bg,
-                color: level.txt,
               }}
             >
               
