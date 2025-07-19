ec8228e6833905caedc0059f3af7b7d335148ce9
amohan179 <157762058+amohan179@users.noreply.github.com>
2025-07-19 12:50:44 -0400

    Fixed MongoDB now that it is fully functional

--- backend/src/main/java/com/example/MyApplication.java
+++ backend/src/main/java/com/example/MyApplication.java
@@ -2,20 +2,33 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+
 
 @RestController
 @SpringBootApplication
 public class MyApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+	// @RequestMapping("/")
+	// String home() {
+	// 	return "Hello World!";
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
 
+    @Bean
+    public CommandLineRunner showMongoUri(@Value("${spring.data.mongodb.uri:NOT SET}") String mongoUri) {
+
+        return args -> {
+            System.out.println("MongoDB URI Loaded" + mongoUri);
+
+        };
+    }
 }
