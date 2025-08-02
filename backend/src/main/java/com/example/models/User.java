f52f7bb94c0d67fd19542d6be3a19c354a334381
Ishita-mathur1 <blossome.jake@gmail.com>
2025-08-02 10:11:48 -0400

    Update User.java

--- backend/src/main/java/com/example/models/User.java
+++ backend/src/main/java/com/example/models/User.java
@@ -9,8 +9,51 @@ public class User {
 
     private String username;
     private String password;
-    private Integer level;
-
+    private Integer level = 0;
     
+    public User() {
+
+    }
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public User(String username, String password, Integer level) {
+        this.level = level;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer get userId() {
+        return userId;
+    }
+
+    public void setUserId( Integer userId) {
+        this.userId = userId
+    }
+
+    public String getUsername(); {
+        return username;
+    }
+
+     public void setUsername( String username); {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPasssword(String password) {
+        this.password = password;
+    }
+   public void increaseLevel( ) {
+    level ++;
+
+   }
+
+
 
 }
